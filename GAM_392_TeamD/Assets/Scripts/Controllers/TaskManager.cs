2cf002d514a2b2cff4f3f23815a27aabc4bc3ade
0041 <34074119+zilinious@users.noreply.github.com>
2021-05-10 16:41:04 -0500

    Fixed same issues
    Git is a dick and reverted all my changes; here they are again

--- GAM_392_TeamD/Assets/Scripts/Controllers/TaskManager.cs
+++ GAM_392_TeamD/Assets/Scripts/Controllers/TaskManager.cs
@@ -85,7 +85,6 @@ public class TaskManager : MonoBehaviour
                 {
                     cpuSprites[i].sprite = indicatorLevels[1];
                 }
-                UnityEngine.Debug.Log(indicatorLevels[1]);
             }
         }
         if(currentCpu / maxCpu >= 1)
