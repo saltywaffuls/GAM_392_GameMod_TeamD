0ee955968002ee65558337f1f1ba41902b39b481
saltywaffuls <tabanpl@gmail.com>
2021-05-13 19:50:52 -0500

    Main Menu

---
+++ GAM_392_TeamD/Assets/scripts/MenuController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+
+    [SerializeField] GameObject install;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Login()
+    {
+        this.gameObject.SetActive(false);
+        install.SetActive(true);
+
+    }
+}
